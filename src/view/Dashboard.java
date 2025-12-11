06be843f2a0d4497b662037158bd96f2663a2e55
Subash Gurung <snewgr@gmail.com>
2025-12-11 07:15:53 +0545

    Abstracted the dashboard and updated the icon names

---
+++ src/view/Dashboard.java
@@ -0,0 +1,31 @@
+package view;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public abstract class Dashboard extends javax.swing.JFrame {
+    protected DashboardPanel dashPanel;
+    public ArrayList<JButton> menuButtonList= new ArrayList<>();
+
+    protected void menuButtonList()
+    {
+        for (MenuComponents item : dashPanel.componentList  )
+        {
+            if(item instanceof MenuItems){
+                if(((MenuItems) item).getComponent() instanceof JButton) {
+                    menuButtonList.add((JButton) (((MenuItems) item).getComponent()));
+                }
+            }
+        }
+
+    }
+
+    public void addMenuButtonListener(ActionListener actionListener)
+    {
+        for(JButton button : menuButtonList)
+        {
+            button.addActionListener(actionListener);
+        }
+    }
+
+}
---
+++ src/view/DashboardPanel.java
@@ -0,0 +1,140 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
+
+public abstract class DashboardPanel extends javax.swing.JPanel{
+    protected javax.swing.JPanel menuPanel;
+    protected javax.swing.JPanel contentPanel;
+    
+    protected javax.swing.JLabel logo;  
+    protected javax.swing.JLabel name;
+    
+    protected final javax.swing.JButton profile;
+    protected final javax.swing.JButton module;
+    protected final javax.swing.JButton attendance;
+    protected final javax.swing.JButton routine;
+    protected final javax.swing.JButton notice;
+    protected final javax.swing.JButton logout;
+    private final Font buttonFont = new Font("Century Gothic",Font.PLAIN,18);
+    private final Dimension buttonDimension;
+    
+    
+    
+    protected List<MenuComponents> componentList = new ArrayList<>();
+    private GroupLayout menuLayout;
+    private GroupLayout contentLayout;
+    
+    
+
+    
+    
+    
+      
+    protected DashboardPanel()    
+    {   
+        this.buttonDimension = new Dimension(210,35);
+        menuPanel= new javax.swing.JPanel();
+        menuLayout = new GroupLayout(menuPanel);
+        menuLayout.setAutoCreateGaps(true);
+        
+     
+        menuPanel.setLayout(menuLayout);
+        menuPanel.setMinimumSize(new Dimension(210,630));
+        menuPanel.setMaximumSize(new Dimension(210,630));
+        menuPanel.setPreferredSize(new Dimension(210,630));
+        //menuPanel.setBackground(Color.blue);
+       menuPanel.setBackground(Color.decode("#308BDE"));
+        
+        
+        contentPanel= new javax.swing.JPanel();
+        contentLayout = new GroupLayout(contentPanel);
+        contentPanel.setLayout(contentLayout);
+        contentPanel.setMinimumSize(new Dimension(1120-210,630));
+        contentPanel.setMaximumSize(new Dimension(1120-210,630));
+        contentPanel.setPreferredSize(new Dimension(1120-210,630));
+        contentPanel.setBackground(Color.white);
+        
+        logo= new javax.swing.JLabel();
+        logo.setText("Logo");
+        logo.setFont( new Font("Century Gothic",Font.PLAIN,30));
+        logo.setForeground(Color.white);
+        logo.setPreferredSize(new Dimension(210,170));
+        logo.setMinimumSize(new Dimension(210,170));
+        logo.setMaximumSize(new Dimension(210,170));
+        
+        name= new javax.swing.JLabel();
+        name.setText("Hardwarica");
+        name.setFont( new Font("Century Gothic",Font.PLAIN,24));
+        name.setForeground(Color.white);
+        name.setPreferredSize(new Dimension(210,40));
+        name.setMinimumSize(new Dimension(210,40));
+        name.setMaximumSize(new Dimension(210,40));
+        
+
+        profile = new javax.swing.JButton();
+        module = new javax.swing.JButton();
+        attendance = new javax.swing.JButton();
+        routine = new javax.swing.JButton();
+        notice = new javax.swing.JButton();
+        logout = new javax.swing.JButton();
+
+      
+        buttonDecorator(profile, "Profile");
+        buttonDecorator(module, "Module");
+        buttonDecorator(attendance, "Attendance");
+        buttonDecorator(routine, "Routine");
+        buttonDecorator(notice, "Notice");
+        buttonDecorator(logout, "Logout");
+    }
+    
+    protected final void menuBuilder(List<MenuComponents> menuComponents)
+    {
+    GroupLayout.ParallelGroup hGroup = menuLayout.createParallelGroup();
+         GroupLayout.SequentialGroup vGroup = menuLayout.createSequentialGroup();
+
+         for (MenuComponents item : menuComponents) {
+             if (item instanceof MenuItems bi) {
+                 hGroup.addComponent(bi.getComponent(), GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE);
+                 vGroup.addComponent(bi.getComponent());
+             } else if (item instanceof MenuGap gi) {
+                 hGroup.addGap(gi.getSize());
+                 vGroup.addGap(gi.getSize());
+             }
+         }
+
+         menuLayout.setHorizontalGroup(hGroup);
+         menuLayout.setVerticalGroup(vGroup);
+
+         revalidate();
+         repaint();
+    }
+    
+    protected final void buttonDecorator(javax.swing.JButton button, String name)
+    {  
+       ImageIcon icon = new ImageIcon(getClass().getResource("/images/"+name.toLowerCase()+".png"));
+       button.setText(name);       
+       button.setFont(buttonFont);
+       button.setPreferredSize(buttonDimension);
+       button.setMinimumSize(buttonDimension);
+       button.setMaximumSize(buttonDimension);
+       button.setContentAreaFilled(true);
+       button.setBackground(new Color(48,139,222,0));
+       button.setOpaque(false);
+       button.setBorderPainted(false);
+       button.setFocusPainted(false);
+       button.setForeground(Color.white);
+       button.setIcon(icon);
+       button.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+       button.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+       button.setIconTextGap(25);
+       button.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+       button.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);       
+    }
+}
+
