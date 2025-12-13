0ec7802ad70a3dbf2160ae5eda71bf2b2bd0f36c
Kumar-Shah10 <shahkumar2061@gmail.com>
2025-12-13 10:29:19 +0545

    Added admindashboard controller

---
+++ src/controller/AdminDashboardController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+
+import view.AdminDashboard;
+
+
+public class AdminDashboardController extends DashboardController{
+    AdminDashboard adminDashboard;
+
+    public AdminDashboardController(AdminDashboard adminDashboard) {
+        this.adminDashboard=adminDashboard;
+        dashboard=adminDashboard;
+        buttonListener();
+
+}
+}
+
