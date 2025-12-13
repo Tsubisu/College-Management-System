1f86a1c3c729e1fab1ecb8188234b50a37bf3b41
Subash Gurung <snewgr@gmail.com>
2025-12-13 08:57:40 +0545

    created abstract class for all dashboard controller

---
+++ src/controller/DashboardController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import view.Dashboard;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public abstract class AdminDashboardController {
+    protected Dashboard dashboard;
+
+    protected void buttonListener(){
+        dashboard.addMenuButtonListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton btn = (JButton) e.getSource();
+                System.out.println(btn.getText() + " clicked");
+                dashboard.getDashPanel().getContentLayout().show(dashboard.getDashPanel().getContentPanel(),btn.getText());
+            }
+        });
+    }
+}
